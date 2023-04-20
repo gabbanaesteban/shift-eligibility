056f43d7fff7aec463cc1ecfef9d7175b3946ab2
Esteban De la Rosa <gabbanaesteban@gmail.com>
2023-04-20 02:32:20 -0400

    Add morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import router from '@src/routes/router';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
