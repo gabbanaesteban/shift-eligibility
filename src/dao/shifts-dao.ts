b17a6f57a00667557acc1f5bdd37286736ea58b4
Esteban De la Rosa <gabbanaesteban@gmail.com>
2023-04-21 03:56:07 -0400

    Fix open shifts query

--- src/dao/shifts-dao.ts
+++ src/dao/shifts-dao.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import type { Shift } from '@prisma/client';
 import prisma from '@src/db';
 import { format } from 'date-fns';
@@ -13,26 +14,28 @@ type GetOpenShiftsByFacilityAndWorkerParams = {
 export function getOpenShiftsByFacilityAndWorker(params: GetOpenShiftsByFacilityAndWorkerParams) {
   const { facilityId, workerId, profession, start, end } = params;
 
+  const formattedStart = format(start, 'yyyy-MM-dd HH:mm:ss');
+  const formattedEnd = format(end, 'yyyy-MM-dd HH:mm:ss');
+
   return prisma.$queryRawUnsafe<Shift[]>(`
-  SELECT
-    s.*
-  FROM
-    "Shift" s
-  WHERE
-    s.facility_id = ${facilityId}
-    AND s.is_deleted = FALSE
-    AND s.worker_id IS NULL
-    AND s.profession = '${profession}'
-    AND s.start >= '${format(start, 'yyyy-MM-dd HH:mm:ss')}'
-    AND s. "end" <= '${format(end, 'yyyy-MM-dd HH:mm:ss')}'
-    AND NOT EXISTS (
-      SELECT
-        1
-      FROM
-        "Shift" claimed_shift
-      WHERE
-        claimed_shift.worker_id = ${workerId}
-        AND claimed_shift.is_deleted = FALSE
-        AND NOT(s. "start" >= claimed_shift. "end"
-          OR s. "end" <= claimed_shift. "start"))`);
+    SELECT
+      s.*
+    FROM
+      "Shift" s
+    WHERE
+      s.facility_id = ${facilityId}
+      AND s.is_deleted = FALSE
+      AND s.worker_id IS NULL
+      AND s.profession = '${profession}'
+      AND tsrange(s.start, s. "end") && tsrange('${formattedStart}', '${formattedEnd}')
+      AND NOT EXISTS (
+        SELECT
+          1
+        FROM
+          "Shift" claimed_shift
+        WHERE
+          claimed_shift.worker_id = ${workerId}
+          AND claimed_shift.is_deleted = FALSE
+          AND tsrange(claimed_shift.start, claimed_shift. "end", '()') && tsrange(s.start, s. "end", '()')
+        )`);
 }
